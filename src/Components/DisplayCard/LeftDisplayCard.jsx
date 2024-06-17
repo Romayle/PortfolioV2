951f950e9906e72deea57b08a88e989ba5adba61
Romayle <romayle.dharmasena@gmail.com>
2024-06-17 21:19:53 +0530

    Hot fix on project routes

--- src/Components/DisplayCard/LeftDisplayCard.jsx
+++ src/Components/DisplayCard/LeftDisplayCard.jsx
@@ -6,7 +6,7 @@ const LeftDisplayCard = ({title, description, link, string}) => {
             <div className="w-full sm:w-1/2 text-black flex flex-col justify-center items-start gap-7 p-8 sm:p-20">
                 <h1 className="text-xl font-bold">{title}</h1>
                 <p className="text-sm tracking-wide">{description}</p>
-                <button className="border-2 py-4 px-8"><Link to={`/projects/${string}`}>View Project</Link></button>
+                <button className="border-2 py-4 px-8"><Link to={`/PortfolioV2/projects/${string}`}>View Project</Link></button>
             </div>
             <img src={link} className="w-full sm:w-1/2"></img>
         </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,25 +11,25 @@ const Home = () => {
                 title={"SIGNWAVE"}
                 description={"Collaborated on the project as part of a second-year group project in developing a mobile application for the deaf community, featuring text to ASL, audio to ASL and video to ASL translation functionalities."} 
                 link={"https://static.wixstatic.com/media/3b2b7f_b57ecf60fa1b4158b878b32f0c5fb70b~mv2.png/v1/fill/w_446,h_494,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Untitled-1.png"}
-                string={"/PortfolioV2/mobileapp"}
+                string={"mobileapp"}
             />
             <RightDisplayCard 
                 title={"TASK MANAGEMENT SYSTEM"}
                 description={"Developed a Task Management Application utilizing the MERN stack (MongoDB, Express, React, and Node.js). This project highlights my proficiency in modern web development technologies through the creation of a dynamic and responsive user interface, robust server-side logic, and efficient data management."}
                 link={"https://static.wixstatic.com/media/3b2b7f_495f1bb0d62a45c4ae5c30bf5aa9bfd7~mv2.png/v1/crop/x_74,y_74,w_1124,h_902/fill/w_589,h_471,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/3b2b7f_495f1bb0d62a45c4ae5c30bf5aa9bfd7~mv2.png"}
-                string={"/PortfolioV2/taskmanagement"}
+                string={"taskmanagement"}
             />
             <LeftDisplayCard 
                 title={"Gallery & Sitemap"}
                 description={"Explore a showcase of my creative skills in a dynamic online format. Navigate through a collection of my work, including website designs, coding projects and creative endeavors."}
                 link={"https://static.wixstatic.com/media/3b2b7f_5c18f2bc463b4142b7350e36915f2a10~mv2.png/v1/crop/x_165,y_3,w_1040,h_973/fill/w_504,h_471,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/gallery.png"}
-                string={"/PortfolioV2/gallery"}
+                string={"gallery"}
             />
             <RightDisplayCard
                 title={"PREVIOUS PORTFOLIO"}
                 description={"My previously developed web portfolio using React and Tailwind CSS showcasing my skills, projects, and experiences. The portfolio features a clean, responsive design optimized for both desktop and mobile devices, ensuring an excellent user experience across various screen sizes."}
                 link={"https://static.wixstatic.com/media/3b2b7f_41dfedb35d8a421a84edb5087ccdc21b~mv2.png/v1/crop/x_226,y_0,w_682,h_771/fill/w_378,h_426,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/gallery%20Images3_0-Recovered-min.png"}
-                string={"/PortfolioV2/previousportfolio"}
+                string={"previousportfolio"}
             />
         </div>
     );
