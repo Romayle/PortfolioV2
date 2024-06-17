d328a3bb28083971acc9878b814889393749fc62
Romayle <romayle.dharmasena@gmail.com>
2024-06-17 21:08:46 +0530

    Last Hot Fix

--- src/Components/DisplayCard/LeftDisplayCard.jsx
+++ src/Components/DisplayCard/LeftDisplayCard.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const LeftDisplayCard = ({title, description, link, string}) => {
     return (
         <div className="w-full bg-white flex flex-col items-center my-6 sm:flex-row">
             <div className="w-full sm:w-1/2 text-black flex flex-col justify-center items-start gap-7 p-8 sm:p-20">
                 <h1 className="text-xl font-bold">{title}</h1>
                 <p className="text-sm tracking-wide">{description}</p>
-                <button className="border-2 py-4 px-8"><a href={`/projects/${string}`}>View Project</a></button>
+                <button className="border-2 py-4 px-8"><Link to={`/projects/${string}`}>View Project</Link></button>
             </div>
             <img src={link} className="w-full sm:w-1/2"></img>
         </div>
--- src/Components/DisplayCard/RightDisplayCard.jsx
+++ src/Components/DisplayCard/RightDisplayCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const  RightDisplayCard = ({title, description, link, string}) => {
     return (
         <div className="w-full bg-white flex flex-col items-center my-6 sm:flex-row">
@@ -5,7 +7,7 @@ const  RightDisplayCard = ({title, description, link, string}) => {
             <div className="w-full sm:w-1/2 text-black flex flex-col justify-center items-start gap-7 p-8 sm:p-20">
                 <h1 className="text-xl font-bold">{title}</h1>
                 <p className="text-sm tracking-wide">{description}</p>
-                <button className="border-2 py-4 px-8 px-8"><a href={`/projects/${string}`}>View Project</a></button>
+                <button className="border-2 py-4 px-8 px-8"><Link to={`/projects/${string}`}>View Project</Link></button>
             </div>
         </div>
     );
