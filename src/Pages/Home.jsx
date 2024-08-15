822b6090965fc9ee4d4f6344bc0e4d8081f62376
Romayle Dharmasena <101620877+Romayle@users.noreply.github.com>
2024-08-15 20:17:15 +0530

    Update img links in Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,31 +10,31 @@ const Home = () => {
             <LeftDisplayCard 
                 title={"SIGNWAVE"}
                 description={"Collaborated on the project as part of a second-year group project in developing a mobile application for the deaf community, featuring text to ASL, audio to ASL and video to ASL translation functionalities."} 
-                link={"https://lh3.googleusercontent.com/drive-viewer/AKGpihbiJOGsCYD_zWRlIQavzLLB7rD6dZgPEK5M8tp_exp9iMDUIS7As4iOykQmbgN131yVIWxIbrmCcj-b2MgVkQNQpQOTtTMWPQ=s1600-rw-v1"}
+                link={"https://lh3.googleusercontent.com/d/1B_fZIXmT8GzjvFOS7jNshNZWD-ZimpKE"}
                 string={"mobileapp"}
             />
             <RightDisplayCard 
                 title={"TASK MANAGEMENT SYSTEM"}
                 description={"Developed a Task Management Application utilizing the MERN stack (MongoDB, Express, React, and Node.js). This project highlights my proficiency in modern web development technologies through the creation of a dynamic and responsive user interface, robust server-side logic, and efficient data management."}
-                link={"https://lh3.googleusercontent.com/drive-viewer/AKGpihYfC7m6-1I9HJqjsIR9wcrQOzKTjgLhEyK-mXmdaMkx1D7ja2tVjK2PqFutEX1f5h-Nj7NZrcf-RKoGEHIs2aAOP_SiGZ-ql5A=s1600-rw-v1"}
+                link={"https://lh3.googleusercontent.com/d/17bpUz7eBG6g-APX5X5VLjGQVTouguMW_"}
                 string={"taskmanagement"}
             />
             <LeftDisplayCard 
                 title={"GALLERY & SITEMAP"}
                 description={"Explore a showcase of my creative skills in a dynamic online format. Navigate through a collection of my work, including website designs, coding projects and creative endeavors."}
-                link={"https://lh3.googleusercontent.com/drive-viewer/AKGpiha0b9OAUuqruLHsEsPc-q6EQ7vu9fTqH7h6-MQ7MVZNrNWjtgL8gTCaKLPKT30UDeePCUhwFCSSNvfHVgMdDVFZcAqfVl9jmKY=s1600-rw-v1"}
+                link={"https://lh3.googleusercontent.com/d/1_nLgc__XpmNVD22xt3ol130lkq7yubXF"}
                 string={"gallery"}
             />
             <RightDisplayCard
                 title={"PREVIOUS PORTFOLIO"}
                 description={"My previously developed web portfolio using React and Tailwind CSS showcasing my skills, projects, and experiences. The portfolio features a clean, responsive design optimized for both desktop and mobile devices, ensuring an excellent user experience across various screen sizes."}
-                link={"https://lh3.googleusercontent.com/drive-viewer/AKGpihZFBHTPV5q7Mxp06oeVbaBPl702uHoSXUSy-pZovve-FWR0ohoryWhj7EP4eu5sNxjYxYzsQrWhA9vxCUPwdpcz0hsd49jj5SM=s1600-rw-v1"}
+                link={"https://lh3.googleusercontent.com/d/1fSg7VFsFsNKIv6vMS0PktJTZsnjNXUYs"}
                 string={"previousportfolio"}
             />
             <LeftDisplayCard 
                 title={"SHOPPING CART SYSTEM"}
                 description={"Created a Java Swing-based Shopping Cart Management System facilitating easy browsing, item selection. Features include inventory management, total calculation, and seamless user interface, demonstrating expertise in Java programming and user-centric design."}
-                link={"https://lh3.googleusercontent.com/drive-viewer/AKGpihYgYUVFv3Gug8g03AM2Hw9G0Jde3VIHWTnWYrWEpuhi-SIVCEJKAuAbCim7KY_N-hcHyUid-EF1sFzbzYJSux-Gw1Xf0HHWkkQ=s1600-rw-v1"}
+                link={"https://lh3.googleusercontent.com/d/1Mpf7P_Bfj1MeU2o1dNLut2PSv1o1CfRi"}
                 string={"shoppingcart"}
             />
         </div>
