7695ce0b6d76414962666d4f098bdae85c3341c8
Romayle Dharmasena <101620877+Romayle@users.noreply.github.com>
2024-08-15 20:14:21 +0530

    Update img links in  Details.jsx

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -5,7 +5,7 @@ const Details = () => {
         <div className="w-full text-black flex flex-col items-center p-8 gap-8 sm:flex-row sm:w-3/5">
             <div className="flex flex-col items-center gap-4">
                 <div className="relative w-32 h-32 rounded-full overflow-hidden">
-                    <img className="w-full h-full object-cover" src="https://lh3.googleusercontent.com/drive-viewer/AKGpihaCEezIWO1UA_BErnk5bRJ_vn0N2gQe3qUOyi5ikNLXWNdmGsWOJk6eLrJIa4cHc6RtNBXCXphczm3EAk4XKzWt8JUfG4cY4vs=s1600-rw-v1" alt="Profile" />
+                    <img className="w-full h-full object-cover" src="https://lh3.googleusercontent.com/d/1xqB4plWvZe7rboSieuhlZXpSge_6Vd6A" alt="Profile" />
                 </div>
                 <div className="flex justify-center gap-4">
                     <a href='https://www.linkedin.com/in/romayle-dharmasena-0ab19326a/'><Icon icon="ri:linkedin-fill" width="32" height="32" style={{color: "black"}} /></a>
